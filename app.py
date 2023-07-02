ecf1f50cb0bfe149d59e8f32490c929159edd088
Snaiel <56014779+Snaiel@users.noreply.github.com>
2023-07-03 00:54:37 +0800

    page formatting

--- app.py
+++ app.py
@@ -2,13 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-with open("input.txt") as file:
+with open("data/input.txt") as file:
     input_text = file.read()
 
 sentences = input_text.split(".")
-sentences = [i.strip().replace("\n", " ") + ". " for i in sentences]
-
 print(sentences)
+sentences = [i + ". " for i in sentences]
 
 @app.route("/")
 def hello_world():

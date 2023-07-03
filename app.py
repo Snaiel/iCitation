922045e07742142fae6c36918337857ef84792e0
Snaiel <56014779+Snaiel@users.noreply.github.com>
2023-07-03 19:52:50 +0800

    form to input text

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
-from flask import Flask, render_template, redirect
-import vector_db
+from flask import Flask, render_template, redirect, request, session
+# import vector_db
 
 app = Flask(__name__)
-
-with open("data/input.txt") as file:
-    input_text = file.read()
-
-sentences = input_text.split(".")
-print(sentences)
-sentences = [i + ". " for i in sentences]
-
-target_sentence = ""
-sources = {}
+app.secret_key = "iCitation"
 
 @app.route("/")
-def hello_world():
-    print("hi", sources)
-    return render_template("base.html", content=sentences, sources=sources, target_sentence=target_sentence)
+def home():
+    return render_template("base.html")
+
+@app.route("/input_text/", methods=['GET', 'POST'])
+def process_text():
+    if request.method == 'POST':
+        text = request.form.get('input_text')
+        text = text.strip()
+        if text[-1] == '.':
+            text = text[:-1]
+        sentences = text.split(".")
+        sentences = [i + ". " for i in sentences]
+        session["sentences"] = sentences
+    return redirect("/")
 
 @app.route('/sentence/<index>')
 def user_profile(index):
-    global sources, target_sentence
-    target_sentence = sentences[int(index)]
-    sources = vector_db.search(target_sentence)
+    sentences = session.get("sentences")
+    session["target_sentence"] = sentences[int(index)]
+    # session["sources"] = vector_db.search(target_sentence)
     return redirect("/")
